4143ed8b2f5aadd6c1e7e8063ade1749a5bc3623
Thiago Scheffer Fialho <86134238+ThiagoSchFialho@users.noreply.github.com>
2023-08-31 20:05:00 -0300

    adição da página 'signup.html'
    Modificações de estilo também foram feitas na página 'index.html', a partial '_header.html' foi tirada do arquivo 'base.html'

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from apps.users.views import login
+from apps.users.views import login, signup
 
 urlpatterns = [
     path('login/', login, name='login'),
+    path('signup', signup, name='signup'),
 ]
--- apps/users/views.py
+++ apps/users/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def login(request):
     return render(request, 'users/login.html')
+
+def signup(request):
+    return render(request, 'users/signup.html');
